c014fcf11cc1d78442c6a12f1906faaaf0813845
Penny Jheng <pennyjheng@z-tron.net>
2019-04-29 13:53:02 +0800

    Revert "The solution of 21, 26, 27, 28, 38"
    This reverts commit 5a181b8ee5788c19c05985fefc7711c6b428d6f4.

--- Algorithms/28-Implement-strStr().js
+++
@@ -1,9 +0,0 @@
-var strStr = function (haystack, needle) {
-  for (var i = 0; ; i++) {
-    for (var j = 0; ; j++) {
-      if (j === needle.length) return i
-      if (i + j === haystack.length) return -1
-      if (needle.charAt(j) !== haystack.charAt(i + j)) break
-    }
-  }
-}
