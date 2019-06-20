6ed4207d69cf6da2f3369c3a400d6fd957fda6e9
Lacrime <penny4313@hotmail.com>
2019-06-20 13:41:01 +0800

    Update the solution of 185

--- Database/185-Department-Top-Three-Salaries.sql
+++ Database/185-Department-Top-Three-Salaries.sql
@@ -12,3 +12,12 @@ WHERE
   IN (0, 1a, 2)
   AND E.DepartmentId = D.Id 
 ORDER BY E.DepartmentId, E.Salary DESC;
+
+# Solution 2
+SELECT D1.Name AS "Department", E1.Name AS "Employee", E1.Salary
+FROM Department D1
+JOIN Employee E1 ON D1.Id = E1.departmentId
+JOIN Department D2 ON D1.Id = D2.Id
+LEFT JOIN Employee E2 ON D2.Id = E2.departmentId AND E1.Salary < E2.Salary
+GROUP BY D1.Name, E1.Name, E1.Salary
+HAVING COUNT(DISTINCT(E2.Salary)) < 3
